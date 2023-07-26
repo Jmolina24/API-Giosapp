53cb365351b411ee5a52757b59457a67ed219e9b
JairMolina <jairmolina51@gmail.com>
2023-07-26 08:39:29 -0500

    se crea un ejemplo

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -18,6 +18,94 @@ router.get('/api/v1/', (req, res) => {
 })
 
 
+router.get('/api/v1/lit-elemt', (req, res) => {
+    const people = [
+        {
+          name: 'Calvin Hawkins',
+          email: 'calvin.hawkins@example.com',
+          phone: '1-770-736-8031',
+          website: 'hildegard.org',          
+          image:
+            'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Kristen Ramos',
+          email: 'kristen.ramos@example.com',
+          phone: '010-692-6593',
+          website: 'hildegard.org',
+          image:
+            'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Ted Fox',
+          email: 'ted.fox@example.com',
+          phone: '1-770-736-8031',
+          website: 'hildegard.org',
+          image:
+            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Tedes Foxas',
+          email: 'ted.foxas@example.com',
+          phone: '151-770-736-8031',
+          website: 'awsgard.org',
+          image:
+          'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Ervin Howell',
+          email: 'Shanna@melissa.tv',
+          phone: '9-770-8031',
+          website: 'anastasia.net',
+          image:
+            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+            name: 'Calvin Hawkins',
+            email: 'calvin.hawkins@example.com',
+            phone: '1-770-736-8031',
+            website: 'hildegard.org',          
+            image:
+              'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          },
+          {
+            name: 'Kristen Ramos',
+            email: 'kristen.ramos@example.com',
+            phone: '010-692-6593',
+            website: 'hildegard.org',
+            image:
+              'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          },
+          {
+            name: 'Ted Fox',
+            email: 'ted.fox@example.com',
+            phone: '1-770-736-8031',
+            website: 'hildegard.org',
+            image:
+              'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          },
+          {
+            name: 'Tedes Foxas',
+            email: 'ted.foxas@example.com',
+            phone: '151-770-736-8031',
+            website: 'awsgard.org',
+            image:
+            'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          },
+          {
+            name: 'Ervin Howell',
+            email: 'Shanna@melissa.tv',
+            phone: '9-770-8031',
+            website: 'anastasia.net',
+            image:
+              'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          }        
+      ]
+    res.json(people);
+})
+
+
+
 
 
 
